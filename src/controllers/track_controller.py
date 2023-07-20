3d65f25276379b40b6378a2d0152a617238fac15
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-07-20 16:25:11 +1000

    Created decorator for is_admin

--- src/controllers/track_controller.py
+++ src/controllers/track_controller.py
@@ -4,9 +4,23 @@ from models.track import Track, track_schema, tracks_schema
 from models.user import User
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from controllers.comment_controller import comments_bp
+import functools
 
 tracks_bp = Blueprint('tracks', __name__, url_prefix='/tracks')
 tracks_bp.register_blueprint(comments_bp, url_prefix='/<int:track_id>/comments')
+
+def authorise_as_admin(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        user_id = get_jwt_identity()
+        stmt = db.select(User).filter_by(id=user_id)
+        user = db.session.scalar(stmt)
+        if user.is_admin:
+            return fn(*args, **kwargs)
+        else:
+            return {'error': 'Not authorised to perform that function. Only an admin has permission.'}, 403
+    
+    return wrapper
 
 @tracks_bp.route('/')
 def get_all_tracks():
@@ -25,11 +39,8 @@ def get_one_track(id):
     
 @tracks_bp.route('/', methods=['POST'])
 @jwt_required()
+@authorise_as_admin
 def create_track():
-    is_admin = authorise_as_admin()
-    if not is_admin:
-        return {'error': 'Not authorised to add tracks.'}, 403
-    
     body_data = track_schema.load(request.get_json(), partial=True)
     
     track = Track(
@@ -49,11 +60,8 @@ def create_track():
 
 @tracks_bp.route('/<int:id>', methods=['DELETE'])
 @jwt_required()
+@authorise_as_admin
 def delete_one_track(id):
-    is_admin = authorise_as_admin()
-    if not is_admin:
-        return {'error': 'Not authorised to delete tracks.'}, 403
-    
     stmt = db.select(Track).filter_by(id=id)
     track = db.session.scalar(stmt)
     if track:
@@ -65,11 +73,8 @@ def delete_one_track(id):
     
 @tracks_bp.route('/<int:id>', methods=['PUT', 'PATCH'])
 @jwt_required()
+@authorise_as_admin
 def update_one_track(id):
-    is_admin = authorise_as_admin()
-    if not is_admin:
-        return {'error': 'Not authorised to update/edit tracks.'}, 403
-    
     body_data = track_schema.load(request.get_json(), partial=True)
     stmt = db.select(Track).filter_by(id=id)
     track = db.session.scalar(stmt)
@@ -85,8 +90,9 @@ def update_one_track(id):
     else:
         return {'error': f'Track not found with id {id}'}, 404
     
-def authorise_as_admin():
-    user_id = get_jwt_identity()
-    stmt = db.select(User).filter_by(id=user_id)
-    user = db.session.scalar(stmt)
-    return user.is_admin
+# def authorise_as_admin():
+#     user_id = get_jwt_identity()
+#     stmt = db.select(User).filter_by(id=user_id)
+#     user = db.session.scalar(stmt)
+#     return user.is_admin
+
