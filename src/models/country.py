67ebbd3ef2ee27494316713ce0f3e07c99e3cde6
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-07-27 12:19:50 +1000

    Created countries and regions models and seeded database

---
+++ src/models/country.py
@@ -0,0 +1,24 @@
+from init import db, ma 
+from marshmallow import fields 
+from marshmallow.validate import Length, And, Regexp
+
+class Country(db.Model):
+    __tablename__ = "countries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String, nullable=False, unique=True)
+
+    regions = db.relationship('Region', back_populates='country', cascade='all, delete')
+
+class CountrySchema(ma.Schema):
+    
+    country = fields.String(required=True, validate=And(
+        Length(min=2, error='Difficulty name must be at least 2 characters long'),
+        Regexp("^[a-zA-Z ]+$", error="Only letters and spaces are allowed")))
+
+    class Meta:
+        fields = ('id', 'country')
+        ordered = True
+
+country_schema = CountrySchema()
+countries_schema = CountrySchema(many=True)
---
+++ src/models/region.py
@@ -0,0 +1,26 @@
+from init import db, ma 
+from marshmallow import fields 
+from marshmallow.validate import Length, And, Regexp
+
+class Region(db.Model):
+    __tablename__ = "regions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    region = db.Column(db.String, nullable=False, unique=True)
+
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable=False)
+
+    country = db.relationship('Country', back_populates='regions')
+
+class RegionSchema(ma.Schema):
+    
+    region = fields.String(required=True, validate=And(
+        Length(min=2, error='Difficulty name must be at least 2 characters long'),
+        Regexp("^[a-zA-Z ]+$", error="Only letters and spaces are allowed")))
+
+    class Meta:
+        fields = ('id', 'region', 'country')
+        ordered = True
+
+region_schema = RegionSchema()
+regions_schema = RegionSchema(many=True)
