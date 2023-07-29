064566a989d8a33682f4de45c89e112563317ad9
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-07-29 12:49:54 +1000

    Added mountain bike type model

---
+++ src/models/mtb_type.py
@@ -0,0 +1,16 @@
+from init import db, ma
+from marshmallow import fields 
+
+class MountainBike(db.Model):
+    __tablename__ = "mountain_bikes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    description = db.Column(db.Text)
+
+class MountainBikeSchema(ma.Schema):
+    fields = ('id', 'name', 'description')
+    ordered = True
+
+mountain_bike_schema = MountainBikeSchema()
+mountain_bikes_schema = MountainBikeSchema(many=True)
